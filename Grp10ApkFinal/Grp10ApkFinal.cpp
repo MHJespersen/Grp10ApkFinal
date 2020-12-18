ee8b75bb92101ed1cc7fe24bef18099ba7b0b2fb
Holsko <Mathiasholsko@hotmail.com>
2020-12-18 11:16:56 +0100

    clean main

--- Grp10ApkFinal/Grp10ApkFinal.cpp
+++ Grp10ApkFinal/Grp10ApkFinal.cpp
@@ -1,29 +1,13 @@
-#include <iostream>
-#include <ostream>
 #include <thread>
 #include "PlaneGenerator.h"
-#include <string>
-#include <string.h>
-#include <mutex>
-#include <stdlib.h>
-#include <vector>
-#include <list>
-#include "Plane.h"
 #include "ControlTower.h"
-#include <boost/uuid/uuid.hpp>
-#include <boost/uuid/uuid_io.hpp>
-#include <boost/uuid/uuid_generators.hpp>
-#include <boost/signals2.hpp>
-#include <boost/bind.hpp>
 
 using namespace std;
 
-
-int main(int argc, const char* argv[])
+void main()
 {
     thread t1(&PlaneGenerator::GeneratePlane, PlaneGenerator());
     thread t2(&ControlTower::CheckAirspace, ControlTower::getInstance());
     t1.join();
     t2.join();
-    return 0;
 }
