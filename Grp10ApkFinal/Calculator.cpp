4570305aaacbbc8810a356df29d3fdcb5ac00fee
Emil Knudsen <eknudsen1992@hotmail.com>
2020-12-22 12:18:23 +0100

    Revert changes to avoid crash

--- Grp10ApkFinal/Calculator.cpp
+++ Grp10ApkFinal/Calculator.cpp
@@ -1,59 +1,59 @@
-#include "Calculator.h"
-
-template<class T>
-float Calculator<T>::distanceCalculator(T* newPlane, T* prevPlane)
-{
-	if constexpr (std::is_pointer_v<Plane*>)
-		return sqrt(pow(prevPlane->xcoordinate - newPlane->xcoordinate, 2) + pow(prevPlane->ycoordinate - newPlane->ycoordinate, 2));
-	else
-		throw(string("Not implemented for this type!\n"));
-}
-
-template<class T>
-float Calculator<T>::speedCalculator(T* newPlane, T* prevPlane)
-{
-	timeStamps = (prevPlane->timestamp - newPlane->timestamp);
-	try
-	{
-		if (timeStamps == 0) throw(string("Divide by zero error!\n"));
-		speed = distanceCalculator(prevPlane, newPlane) / timeStamps;
-
-	}
-	catch (const string& e)
-	{
-		cout << "exception caught\n" << e;
-	}
-	if (speed < 0)
-	{
-		speed *= -1;
-		return speed * 315;
-	}
-	else
-	{
-		return speed * 315;
-	}
-}
-
-template<class T>
-float Calculator<T>::courseCalculator(T* newPlane, T* prevPlane)
-{
-	courseInRadians = atan2(newPlane->xcoordinate - prevPlane->xcoordinate, newPlane->ycoordinate - prevPlane->ycoordinate);
-	try
-	{
-		if (((180 / pi) * courseInRadians) == 0) throw(string("Divide by zero error!\n"));
-		degrees = (180 / pi) * courseInRadians;
-	}
-	catch (const string& e)
-	{
-		cout << "Exception caught\n" << e;
-	}
-	if (degrees < 0)
-	{
-		degrees = degrees + 360;
-		return degrees;
-	}
-	else
-	{
-		return degrees;
-	}
-}
+//#include "Calculator.h"
+//
+//template<class T>
+//float Calculator<T>::distanceCalculator(T* newPlane, T* prevPlane)
+//{
+//	if constexpr (std::is_pointer_v<Plane*>)
+//		return sqrt(pow(prevPlane->xcoordinate - newPlane->xcoordinate, 2) + pow(prevPlane->ycoordinate - newPlane->ycoordinate, 2));
+//	else
+//		throw(string("Not implemented for this type!\n"));
+//}
+//
+//template<class T>
+//float Calculator<T>::speedCalculator(T* newPlane, T* prevPlane)
+//{
+//	timeStamps = (prevPlane->timestamp - newPlane->timestamp);
+//	try
+//	{
+//		if (timeStamps == 0) throw(string("Divide by zero error!\n"));
+//		speed = distanceCalculator(prevPlane, newPlane) / timeStamps;
+//
+//	}
+//	catch (const string& e)
+//	{
+//		cout << "exception caught\n" << e;
+//	}
+//	if (speed < 0)
+//	{
+//		speed *= -1;
+//		return speed * 315;
+//	}
+//	else
+//	{
+//		return speed * 315;
+//	}
+//}
+//
+//template<class T>
+//float Calculator<T>::courseCalculator(T* newPlane, T* prevPlane)
+//{
+//	courseInRadians = atan2(newPlane->xcoordinate - prevPlane->xcoordinate, newPlane->ycoordinate - prevPlane->ycoordinate);
+//	try
+//	{
+//		if (((180 / pi) * courseInRadians) == 0) throw(string("Divide by zero error!\n"));
+//		degrees = (180 / pi) * courseInRadians;
+//	}
+//	catch (const string& e)
+//	{
+//		cout << "Exception caught\n" << e;
+//	}
+//	if (degrees < 0)
+//	{
+//		degrees = degrees + 360;
+//		return degrees;
+//	}
+//	else
+//	{
+//		return degrees;
+//	}
+//}
